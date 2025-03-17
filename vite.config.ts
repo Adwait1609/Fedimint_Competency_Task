78393fd37218071f16db233854320eae12b726b7
Adwait Kadam <adwaitvk22@iitk.ac.in>
2025-03-17 16:02:03 +0530

    Initial commit - Fedimint Web Wallet

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite'
+import wasm from 'vite-plugin-wasm'
+import topLevelAwait from 'vite-plugin-top-level-await' // Optional
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    wasm(),
+    topLevelAwait(), // Optional
+  ],
+
+  worker: {
+    format: 'es',
+    plugins: () => [
+      wasm(),
+      topLevelAwait(), // Optional
+    ],
+  },
+
+  optimizeDeps: {
+    exclude: ['@fedimint/core-web'],
+  },
+})
